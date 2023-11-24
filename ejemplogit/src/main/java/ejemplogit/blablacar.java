0508791ec6c41349262d9b67163956e5bb75036d
Alumno_Presencial <Alumno_Presencial@DESKTOP-B44B142>
2023-11-24 09:29:17 +0100

    interfaces agregadas

---
+++ ejemplogit/src/main/java/ejemplogit/blablacar.java
@@ -0,0 +1,5 @@
+package ejemplogit;
+
+public interface blablacar {
+
+}
--- ejemplogit/src/main/java/ejemplogit/ejemplo1.java
+++ ejemplogit/src/main/java/ejemplogit/ejemplo1.java
@@ -4,7 +4,13 @@ public class ejemplo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola git");        
+		System.out.println("Hola git");     
+		
+		System.out.println("Ya se usar git");
+		
+		String mivariable;
+		String mientero;
+		
 	}
 
 }
